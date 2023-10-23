5cb4461a25e932e7532adda5714ea3437d751c3d
Godwin847 <g.nduka-2022@hull.ac.uk>
2023-10-23 09:49:58 +0100

    pushing files now

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Calculations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return a / b;
+        }
+
+
+    }
+
+}
